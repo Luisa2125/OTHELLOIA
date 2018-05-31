00ced9a1763a559624a6501b638d93a90ac21617
luisa2125 <luiyi2125@gmail.com>
2018-05-30 18:07:14 -0600

    ya presente, penultimo

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 
 var TreeNode = require('treenode').TreeNode;
-/*var readline = require('readline-sync');
+var readline = require('readline-sync');
 var user = readline.question('ingrese el nombre que quiera: ');
-var port = 3000
-var tournamentID = 12
+var port = 4000
+var tournamentID = 142857
 var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-var socket = require('socket.io-client')("http://localhost:"+ port +"");
+var socket = require('socket.io-client')("http://192.168.1.142:"+ port +"");
 
 var connect = socket.on('connect', function(){
     console.log("you are connected to the server")
@@ -37,6 +37,7 @@ socket.on('ready', function(data){
   
 });
 var pos = []
+var used = []
 var f = 0
 socket.on('ready', function(data){
   
@@ -45,9 +46,9 @@ socket.on('ready', function(data){
   var board = data.board;
   var i=0;
   printBoard(board)
-  var mov = getMov(board,playerTurnID);
-  
-  
+  //var mov = getMov(board,playerTurnID);
+  var mov = getMoveMM(board,playerTurnID)
+  used.push(mov)
   //console.log("mov",mov);
   
   
@@ -74,17 +75,23 @@ socket.on('finish', function(data){
     player_turn_id: playerTurnID,
     game_id: gameID
   });
-});*/
-var actual = [0,0,0,0,0,0,0,0,
-             0,0,0,0,0,0,0,0,
-             0,0,0,0,0,0,0,0,
-             0,0,0,2,1,0,0,0,
-             0,0,0,1,2,0,0,0,
-             0,0,0,0,0,0,0,0,
-             0,0,0,0,0,0,0,0,
-             0,0,0,0,0,0,0,0]
+});/*
+var actual = [
+0 , 0 , 0 , 0 , 0 , 0 , 0 , 0,
+0 , 0 , 0 , 0 , 0 , 0 , 0 , 0,
+0 , 0 , 0 , 0 , 0 , 0 , 0 , 0,
+0 , 0 , 0 , 2 , 1 , 0 , 0 , 0,
+0 , 0 , 0 , 1 , 2 , 0 , 0 , 0,
+0 , 0 , 0 , 0 , 0 , 0 , 0 , 0,
+0 , 0 , 0 , 0 , 0 , 0 , 0 , 0,
+0 , 0 , 0 , 0 , 0 , 0 , 0 , 0,
+]
+*/
+//printBoard(actual)
+//console.log(boardMove(actual,2))
+//console.log(getMoveMM(actual,1))
 //console.log(board[28])
-var playerTurnID = 1
+//var playerTurnID = 1
 
 
 //var n = 0
@@ -121,6 +128,7 @@ function boardMove(board, playerTurnID){
 			if(board[i-1]==0){
 				//console.log('l',i-1,board[i-1])
 				//movements['left'] = i-1;
+				//console.log('> ',i-1,' ',verifyLeft(i,playerTurnID,board),'-->',i)
 				if(verifyLeft(i,playerTurnID,board) && movements.indexOf(i-1) == -1){
 					movements.push(i-1);
 				}
@@ -128,6 +136,7 @@ function boardMove(board, playerTurnID){
 			if(board[i-8]==0){
 				//console.log('l',i-8,board[i-8])
 				//movements['up'] = i-8;
+				//console.log('> ',i-8,' ',verifyUp(i,playerTurnID,board),'-->',i)
 				if(verifyUp(i,playerTurnID,board) && movements.indexOf(i-8) == -1){
 					movements.push(i-8);
 				}
@@ -137,6 +146,7 @@ function boardMove(board, playerTurnID){
 			if(board[i-9] == 0){
 				//console.log('l',i-9,board[i-9])
 			//	movements['upleft'] = n;
+				//console.log('> ',i-9,' ',leftUp(i,playerTurnID,board),'-->',i)
 				if(leftUp(i,playerTurnID,board) && movements.indexOf(i-9) == -1){
 					movements.push(i-9);
 				}
@@ -144,6 +154,7 @@ function boardMove(board, playerTurnID){
 			if(board[i-7] == 0){
 				//console.log('l',i-7,board[i-7]) 
 				//movements['upright'] = n+2;
+				//console.log('> ',i-7,' ',rightUp(i,playerTurnID,board),'-->',i)
 				if(rightUp(i,playerTurnID,board) && movements.indexOf(i-7) == -1){
 					movements.push(i-7);
 				}
@@ -151,6 +162,7 @@ function boardMove(board, playerTurnID){
 			if(board[i+1]==0){
 				//console.log('l',i+1,board[i+1])
 				//movements['righ'] = i+1;
+				//console.log('> ',i+1,' ',verifyRight(i,playerTurnID,board),'-->',i)
 				if(verifyRight(i,playerTurnID,board) && movements.indexOf(i+1) == -1){
 					movements.push(i+1);
 				}
@@ -158,6 +170,7 @@ function boardMove(board, playerTurnID){
 			if(board[i+8]==0){
 				//console.log('l',i+8,board[i+8])
 				//movements['down'] = i+8;
+				//console.log('> ',i+8,' ',verifyDown(i,playerTurnID,board),'-->',i)
 				if(verifyDown(i,playerTurnID,board) && movements.indexOf(i+8) == -1){
 					movements.push(i+8);
 				}
@@ -165,6 +178,7 @@ function boardMove(board, playerTurnID){
 			if(board[i+7] == 0){
 				//console.log('l',i+7,board[i+7])
 			//	movements['downleft'] = n;
+				//console.log('> ',i+7,' ',leftDown(i,playerTurnID,board),'-->',i)
 				if(leftDown(i,playerTurnID,board) && movements.indexOf(i+7) == -1){
 					movements.push(i+7);
 				}
@@ -172,7 +186,8 @@ function boardMove(board, playerTurnID){
 			if(board[i+9] == 0){
 				//console.log('l',i+9,board[i+9])
 				//movements['downrigh'] = n+2;
-				if(rightUp(i,playerTurnID,board) && movements.indexOf(i+9) == -1){
+				//console.log('> ',i+9,' ',rightDown(i,playerTurnID,board),'-->',i)
+				if(rightDown(i,playerTurnID,board) && movements.indexOf(i+9) == -1){
 					movements.push(i+9);
 				}
 			}
@@ -189,14 +204,15 @@ function boardMove(board, playerTurnID){
 	//value = movem[key]
 	//console.log('value',movem, turn,playerTurnID)
 	//console.log(movements)
-
-	return movements
+	
+		//console.log(movements)
+		return movements
+	
 }
 //------------- ELECCION ALEATORIA DE MOVMIENTOS--------------------------
 function getMov(board,playerTurnID){
 	var movement = boardMove(board,playerTurnID)
-	return movement[Math.floor(Math.random()*movement.length)]
-	
+	return movement[Math.floor(Math.random()*movement.length)]	
 }
 //---------------VERIFICACIONES DE MOVMIENTOS---------------------
 function verifyUp(pos,turn,board){
@@ -217,7 +233,6 @@ function verifyUp(pos,turn,board){
 }
 function verifyLeft(pos,turn,board){
 	var i = pos;
-	
 	var make = false;
 	while(i<=63 && !make && board[i] != 0){
 		i += 1;
@@ -229,11 +244,11 @@ function verifyLeft(pos,turn,board){
 		}
 		
 	}
+
 	return make;
 }
 function verifyRight(pos,turn,board){
 	var i = pos;
-	
 	var make = false;
 	while(i>=0 && !make && board[i] != 0){
 		i -= 1;
@@ -245,12 +260,14 @@ function verifyRight(pos,turn,board){
 		}
 		
 	}
+
+	
 	return make;
 }
 function verifyDown(pos,turn,board){
 	var i = pos;
-	
 	var make = false;
+
 	while(i>=0 && !make && board[i] != 0){
 		i -= 8;
 		
@@ -282,6 +299,7 @@ function leftUp(pos,turn,board){
 			n+=1
 		}
 	}
+	
 	return make;
 }
 function rightUp(pos,turn,board){
@@ -299,8 +317,9 @@ function rightUp(pos,turn,board){
 			make = false
 			n+=1
 		}
-		
 	}
+		
+	
 	return make;
 }
 function leftDown(pos,turn,board){
@@ -319,8 +338,9 @@ function leftDown(pos,turn,board){
 			make = false
 			n+=1
 		}
-		//console.log('dentro',pos,make)
 	}
+		//console.log('dentro',pos,make)
+	
 	//console.log('fuera',pos,make)
 	return make;
 }
@@ -330,7 +350,7 @@ function rightDown(pos,turn,board){
 	var make = false;
 	while(i>=0 && !make && board[i] != 0){
 		i -= 9;
-		//console.log(i,make,board[i])
+		//console.log(pos,i,make,board[i])
 		if(board[i] != turn){
 			make = false;
 			n+=1
@@ -342,8 +362,8 @@ function rightDown(pos,turn,board){
 			n+=1
 		}
 		
-	
 	}
+
 	return make;
 }
 //------------CAMBIAR EL BOARD --------------------------------
@@ -363,7 +383,7 @@ function boardchange(turn,bboard,mov){
 		var limit = true;
 		var side = [verifyUp(mov,turn,board),verifyDown(mov,turn,board),verifyLeft(mov,turn,board),verifyRight(mov,turn,board),leftUp(mov,turn,board),rightUp(mov,turn,board),leftDown(mov,turn,board),rightDown(mov,turn,board)]
 		board[mov] = 0;
-		console.log(side)
+		//console.log(side)
 		if(side[0]){
 			board = changeUP(board,mov,turn)
 		}if(side[1]){
@@ -500,12 +520,12 @@ function nodeTree(board, playerTurnID,level,tableros,childs){
 
 	for(var i =0;i<movs.length;i++){
 
-		console.log('level', level)
-		console.log('mov ', movs[i], movs)
+		//console.log('level', level)
+		//console.log('mov ', movs[i], movs)
 		new_board = boardchange(playerTurnID,actual_board,movs[i])
-		printBoard(new_board)
+		//printBoard(new_board)
 		//console.log(node_num)
-		console.log('h',i,childs)
+		//console.log('h',i,childs)
 		node_num += 1
 		childs.push(node_num)
 		if(i == movs.length -1){
@@ -513,8 +533,8 @@ function nodeTree(board, playerTurnID,level,tableros,childs){
 			n+=1
 			var childs = []
 		}
-		console.log('childs',childs)
-		console.log('this',node_num)
+		//console.log('childs',childs)
+		//console.log('this',node_num)
 		tableros[node_num] = new_board
 		actual_board = board.slice();
 		if (level < 2){
@@ -533,103 +553,343 @@ console.log(node_num)
 console.log(arbol)
 */
 
-var movs = boardMove(actual, 1)
-printBoard(actual)
-var tree = new TreeNode({id:0, name:'root',board:actual,movements:movs,turn:playerTurnID,value:0})
-var n = 0
-var level = 1
 
-function makeTree(node,n,level){
-	n+=1
+function makeTree(node,n){
+	n=1
 	
 	var turn = node.data.turn
-	console.log('TURN',turn)
+	var op = 0
+
+	if(turn == 1){
+		op = 2
+	}else{
+		op = 1
+	}
+	//console.log('TURN',turn)
 	for(var i = 0;i<node.data.movements.length;i++){
 		var board = boardchange(turn,node.data.board,node.data.movements[i])
-		console.log('before')
-		printBoard(node.data.board)
-		var movements = boardMove(board, 2)
+		//console.log('before')
+		//printBoard(node.data.board)
+		var movements = boardMove(board, op)
 		var name  = "node "+i+"-"+node.data.movements[i]
-		var new_node = node.addChild({id:n, name: name,board:board,movements:movements,turn:turn,value:0})
-		console.log('after')
-		printBoard(new_node.data.board)
+		//console.log('n',n)
+		var new_node = node.addChild({id:n, name: name,board:board,movements:movements,turn:turn,value:0,actualM:node.data.movements[i]})
+		//console.log('after',new_node.data.id)
+		//printBoard(new_node.data.board)
 
 	}
 
 	if(turn == 1){
 		turn = 2
+		op = 1
 	}else{
 		turn = 1
+		op = 2
 	}
-	console.log('------------------------------------------------------------')
-	console.log('TURN',turn)
+	//console.log('------------------------------------------------------------')
+	//console.log('TURN',turn)
 	n+=1
-	tree.forEach(function(element) {
-		if(element.data.id ==1){
-			 
+	node.forEach(function(element) {
+		if(element.data.id ==1 && element.data.actualM != 'no'){ 
 			for(var i = 0;i<element.data.movements.length;i++){
 				var board = boardchange(turn,element.data.board,element.data.movements[i])
-				console.log('before')
-				printBoard(element.data.board)
-				var movements = boardMove(board, 1)
+				//console.log('before')
+				//printBoard(element.data.board)
+				var movements = boardMove(board, op)
 				var name  = "node "+i+"-"+element.data.movements[i]
-				var new_node = element.addChild({id:n, name: name,board:board,movements:movements,turn:turn,value:0})
-				console.log('after')
-				printBoard(new_node.data.board)
+				var new_node = element.addChild({id:n, name: name,board:board,movements:movements,turn:turn,value:0,actualM:element.data.movements[i]})
+				//console.log('after', new_node.data.id)
+				//printBoard(new_node.data.board)
 
 			} 
-			console.log(element.data.name,element.numChildren())
+			//console.log(element.data.name,element.numChildren())
 		}
 			
  	})
  	if(turn == 1){
 		turn = 2
+		op = 1
 	}else{
 		turn = 1
+		op = 2
 	}
-	console.log('------------------------------------------------------------')
-	console.log('TURN2',turn)
-	tree.forEach(function(element) {
-		if(element.data.id ==2){
-			n+=1 
+	//console.log('------------------------------------------------------------')
+	//console.log('TURN2',turn)
+	n+=1
+	node.forEach(function(element) {
+		if(element.data.id ==2 && element.data.actualM != 'no'){
+			 
 			for(var i = 0;i<element.data.movements.length;i++){
 				var board = boardchange(turn,element.data.board,element.data.movements[i])
-				console.log('before')
-				printBoard(element.data.board)
-				var movements = boardMove(board, 2)
+				//console.log('before')
+				//printBoard(element.data.board)
+				var movements = boardMove(board, op)
 				var name  = "node "+i+"-"+element.data.movements[i]
-				var new_node = element.addChild({id:n, name: name,board:board,movements:movements,turn:turn})
-				console.log('after')
-				printBoard(new_node.data.board)
+				var new_node = element.addChild({id:n, name: name,board:board,movements:movements,turn:turn,value:0,actualM:element.data.movements[i]})
+				//console.log('after', new_node.data.id)
+				//printBoard(new_node.data.board)
 
 			} 
-			console.log(element.data.name,element.numChildren())
+			//console.log(element.data.name,element.numChildren())
 		}
 		
 	})
+	if(turn == 1){
+		turn = 2
+		op = 1
+	}else{
+		turn = 1
+		op = 2
+	}
+	//console.log('------------------------------------------------------------')
+	//console.log('TURN2',turn)
+	n+=1
+	node.forEach(function(element) {
+		if(element.data.id ==3 && element.data.actualM != 'no'){
+			 
+			for(var i = 0;i<element.data.movements.length;i++){
+				var board = boardchange(turn,element.data.board,element.data.movements[i])
+				//console.log('before')
+				//printBoard(element.data.board)
+				var movements = boardMove(board, op)
+				var name  = "node "+i+"-"+element.data.movements[i]
+				var new_node = element.addChild({id:n, name: name,board:board,movements:movements,turn:turn,value:0,actualM:element.data.movements[i]})
+				//console.log('after', new_node.data.id)
+				//printBoard(new_node.data.board)
+
+			} 
+			//console.log(element.data.name,element.numChildren())
+		}
+		
+	})
+	if(turn == 1){
+		turn = 2
+		op = 1
+	}else{
+		turn = 1
+		op = 2
+	}
+	//console.log('------------------------------------------------------------')
+	//console.log('TURN2',turn)
+	n+=1
+	node.forEach(function(element) {
+		if(element.data.id ==4 && element.data.actualM != 'no'){
+			 
+			for(var i = 0;i<element.data.movements.length;i++){
+				var board = boardchange(turn,element.data.board,element.data.movements[i])
+				//console.log('before')
+				//printBoard(element.data.board)
+				var movements = boardMove(board, op)
+				var name  = "node "+i+"-"+element.data.movements[i]
+				var new_node = element.addChild({id:n, name: name,board:board,movements:movements,turn:turn,value:0,actualM:element.data.movements[i]})
+				//console.log('after', new_node.data.id)
+				//printBoard(new_node.data.board)
+
+			} 
+			//console.log(element.data.name,element.numChildren())
+		}
+		
+	})
+	
 	//console.log('num',tree.numChildren())
 
 }
-makeTree(tree,n,level)
-console.log('num',tree.numChildren())
-var count  = 0
-tree.forEach(function(element) {
-	count+=1
- });
-console.log('count ',count)
+
+
+function poda_alfa_beta(level,root,turn){
+	var move = -1
+	var actual_best = -1
+	root.forEach(function(element){
+		if(element.data.id == level){
+			var value_move = Value(element.data.board,element.data.turn)
+			//console.log(actual_best,value_move)
+			if(value_move>actual_best){
+				actual_best = value_move
+				element.data.value = value_move
+			}else{
+				element.data.value = 'x'
+			}
+			
+			//console.log('value',level,"--",element.data.value)
+		}
+	})
+	level -=1
+	console.log('best',actual_best)
+	while(level>=0){
+		root.forEach(function(element){
+			if(element.data.id == level){
+				var value_move = Value(element.data.board,element.data.turn)
+				//console.log(actual_best,value_move)
+				if(level%2 == 0){
+					if(value_move>actual_best){
+						actual_best = value_move
+						element.data.value = value_move
+					}else{
+						element.data.value = 'x'
+					}
+				}else{
+					if(value_move<actual_best){
+						actual_best = value_move
+						element.data.value = value_move
+					}else{
+						element.data.value = 'x'
+					}
+				}
+				
+				
+				//console.log('value',level,"--",element.data.value)
+			}
+
+		})
+		level-=1
+		console.log('best',actual_best)
+	}
+	root.forEach(function(element){
+		if(element.data.id == 1 ){
+			console.log(element.data.actualM)
+			if(root.data.value == element.data.value){
+				move = element.data.actualM
+			}
+		}
+	})
+
+	console.log('move',move)
+	if(move == -1){
+		move = getMov(root.data.board,root.data.playerTurnID)
+	}
+	return move;
+	
+}
+function getMoveMM(board,playerTurnID){
+	var movs = boardMove(board, playerTurnID)
+	//printBoard(board)
+	var tree = new TreeNode({id:0, name:'root',board:board,movements:movs,turn:playerTurnID,value:0,actualM:-1})
+	var n = 0
+	var level = 0
+
+	makeTree(tree,n,level)
+	var count  = 0
+	var level = 0
+	tree.forEach(function(element) {
+		count+=1
+		level = element.data.id
+	});
+
+	console.log('count ',count,' level ',level)
+	//return poda_alfa_beta(level,tree,tree.data.turn)
+	return MinMax(level,tree,1)
+
+}
+function MinMax(level,root,turn){
+	var move = -1
+	root.forEach(function(element){
+		if(element.data.id == level){
+			var value_move = Value(element.data.board,element.data.turn)
+			element.data.value = value_move
+			//console.log('value',level,"--",element.data.value)
+		}
+	})
+	level -= 1
+	while(level>=0){
+		root.forEach(function(element){
+			if(element.data.id == level){
+				var value_move = 0
+				//console.log(level%2)
+				if(level%2 == 0){
+					value_move = MaxMove(element,level)
+				}else{
+					value_move = MinMove(element,level)
+				}
+
+				element.data.value = value_move
+				//console.log('value',level,"--",element.data.value)
+			}
+		})
+		level-=1
+	}
+	root.forEach(function(element){
+		if(element.data.id == 1 ){
+
+			console.log(element.data.actualM)
+			if(root.data.value == element.data.value){
+				move = element.data.actualM
+			}
+		}
+	})
+	console.log(move)
+	return move
+}
+function MinMove(this_node,level){
+	var value = 0
+	this_node.forEach(function(element){
+		if(element.data.id == level+1){
+			//console.log('min ',element.data.value)
+			if(value == 0){
+				value = element.data.value
+			}else
+			if(element.data.value < value){
+				value = element.data.value
+			}
+		}
+	})
+	return value
+}
+function MaxMove(this_node,level){
+	var value = 0
+	this_node.forEach(function(element){
+		if(element.data.id == level+1){
+			//console.log('max ',element.data.value)
+			if(value == 0){
+				value = element.data.value
+			}else
+			if(element.data.value > value){
+				value = element.data.value
+			}
+		}
+	})
+	return value
+}
+function Value(board,turn){
+	return heuristica_esquinas(board,turn) + heuristica_count(board,turn)
+}
+function heuristica_esquinas(board,turn){
+	var count = 0;
+	if(board[0] == turn){
+		count +=1;
+	}if(board[7] == turn){
+		count +=1;
+	}if(board[63] == turn){
+		count +=1
+	}if(board[56] == turn){
+		count +=1
+	}
+	return count;
+}
+function heuristica_count(board,turn){
+	var count = 0;
+	for(var i = 0;i<board.length;i++){
+		if(board[i] == turn){
+			count +=1
+		}
+	}
+	return count;
+}
+
+//console.log('heuristica esquinas',heuristica_esquinas(actual,1),' heuristica count',heuristica_count(actual,1))
+
 
 /*  function MinMax(game){
  	return MaxMove(game)
  }
  function MaxMove(game){
- 	console.log('max',game[1])
+ 	//console.log('max',game[1])
  	//printBoard(game[0])
  	if(GameEnded(game)){
  		return EvalGameState(game);
  	}else{
  		var best_move = [];
  		var moves = GenerateMoves(game);
- 		console.log(moves)
+ 		//console.log(moves)
  		for(var i = 0;i<moves.length;i++){
  			game[2] = moves[i]
  			var applyMove = ApplyMove(game);
@@ -642,11 +902,11 @@ console.log('count ',count)
  	}
  }
  function MinMove(game){
- 	console.log('min',game[1])
+ 	//console.log('min',game[1])
  	//printBoard(game[0])
  	var best_move = [];
  	var moves = GenerateMoves(game)
- 	console.log(moves)
+ 	//console.log(moves)
  	for(var i = 0;i<moves.length;i++){
 		var applyMove = ApplyMove(game);
 		var move = MaxMove(applyMove);
@@ -693,9 +953,9 @@ console.log('count ',count)
  		game[1] = 2
  	}
  	game[2] = 'finish'
- 	console.log(game[2])
- 	console.log('player ',game[1],' won')
- 	printBoard(game[0])
+ 	//console.log(game[2])
+ 	//console.log('player ',game[1],' won')
+ 	//printBoard(game[0])
  	return game 
  }
  function Value(move){
